c8a971072fc98c8c7a1a77f07d37b66de5d54aa1
Missy May <missysuemay@gmail.com>
2014-07-10 22:07:56 -0400

    Initial Check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewYorkTimesEvents::Application.config.secret_key_base = 'fcb8931f0db2b6a8665544a8a3ec0e93914e70767e7192a3e9de46381ebb8698d448dbec273ab2ed7e540b59e6112b0718cdf153f4e15c74bf255478a917c6aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewYorkTimesEvents::Application.config.session_store :cookie_store, key: '_new_york_times_events_session'
